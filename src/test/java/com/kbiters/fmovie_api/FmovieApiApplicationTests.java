7a46a0986922e8773fa14f5f7be92d36a686d042
Juan Pablo <juampigarcia2014@gmail.com>
2021-04-30 22:55:55 -0300

    feat: Setup Spring Boot

---
+++ src/test/java/com/kbiters/fmovie_api/FmovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbiters.fmovie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
