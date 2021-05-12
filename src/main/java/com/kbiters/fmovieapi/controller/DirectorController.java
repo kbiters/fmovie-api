5e3548669fba92426b0fc202c3ddc3ef8f3f4441
Juan Pablo <juampigarcia2014@gmail.com>
2021-05-11 21:06:40 -0300

    fix: Update DirectorController

--- src/main/java/com/kbiters/fmovieapi/controller/DirectorController.java
+++ src/main/java/com/kbiters/fmovieapi/controller/DirectorController.java
@@ -4,20 +4,17 @@ package com.kbiters.fmovieapi.controller;
 import com.kbiters.fmovieapi.model.DirectorModel;
 import com.kbiters.fmovieapi.service.DirectorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/director")
+@RequestMapping("/directors")
 public class DirectorController {
     @Autowired
     DirectorService directorService;
 
-
+    @GetMapping()
     public ArrayList<DirectorModel> getDirectors() {
         return directorService.getDirectors();
     }
