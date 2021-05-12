8e61d9c4923a7d5887079fee999dfcb1267a5509
gianca1994 <gianca9405@gmail.com>
2021-05-12 17:11:34 -0300

    Http Methods Director

--- src/main/java/com/kbiters/fmovieapi/service/DirectorService.java
+++ src/main/java/com/kbiters/fmovieapi/service/DirectorService.java
@@ -14,11 +14,30 @@ public class DirectorService {
     @Autowired
     DirectorRepository directorRepository;
 
+    
     public ArrayList<DirectorModel> getDirectors() {
         return (ArrayList<DirectorModel>) directorRepository.findAll();
     }
 
     public DirectorModel saveDirector(DirectorModel director) {
         return directorRepository.save(director);
+    }
+
+    public DirectorModel getDirector(Long id) {
+        return directorRepository.findById(id);
+    }
+
+    public DirectorModel updateDirector(DirectorModel newDirector, Long id) {
+        return directorRepository.findById(id).map(director -> {
+            director.setName(newDirector.getName());
+            return directorRepository.save(director);
+        }).orElseGet(() ->{
+            newDirector.setId(id);
+            return directorRepository.save(newDirector);
+        });
+    }
+
+    public DirectorModel deleteDirector(Long id) {
+        return directorRepository.deleteById(id);
     }
 }
